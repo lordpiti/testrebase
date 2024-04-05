94154c03767c46a5f255e4d8aba40994deb628db
Pablo Amil <lordpiti@gmail.com>
2024-04-05 01:22:46 +0100

    all deleted

--- testrebase/testrebase/Program.cs
+++ testrebase/testrebase/Program.cs
@@ -1,5 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World2!");
-Console.WriteLine("Hello, WorldMAIN1!");
-Console.WriteLine("MAIN SUPERUPDATED");
+ITS ALL DELETED IN THE TEST BRANCH
+
