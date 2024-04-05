6133d90090fa8de2493c38e10e88003f0513484f
Pablo Amil <lordpiti@gmail.com>
2024-04-05 01:39:09 +0100

    new content main

--- testrebase/testrebase/Program.cs
+++ testrebase/testrebase/Program.cs
@@ -1,6 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World2!");
-Console.WriteLine("Hello, WorldMAIN1!");
-Console.WriteLine("MAIN SUPERUPDATED");
-THIS SHOULD BE IGNORED
+﻿new content in main
